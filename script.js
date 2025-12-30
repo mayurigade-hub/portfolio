86111e4d0059cb0d25d5c4a9d9553063c44d3891
Mayuri Gade <mayurigade569@gmail.com>
2025-12-30 09:57:16 +0530

    first commit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // toggle icon navbar
 
 let menuIcon = document.querySelector('#menu-icon');
-let navbar =  document.querySelector('.navbar');
+let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-xmark');
     navbar.classList.toggle('active');
-}  
+}
 
 // scroll section active link
 
@@ -20,20 +20,20 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
-            navlinks.forEach.apply(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector(`header nav a[href*="${id}"]`).classList.add('active');
             });
         };
     });
-// Sticky Navbar
-let header = document.querySelectorAll('header');
-header.classList.toggle('sticky', window.scrollY > 100);
+    // Sticky Navbar
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-// Remove toggle icon and navbar
-menuIcon.classList.remove('fa-mark');
-navbar.classList.remove('active');
+    // Remove toggle icon and navbar
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
 };
 
 // scroll Reveal
@@ -44,17 +44,17 @@ ScrollReveal({
     delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'button'})
-ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left'})
-ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right'})
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'button' })
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' })
+ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' })
 
 // typed JS
 
 const typed = new Typed('.multiple-text', {
     strings: ['Web Developer', 'Engineering Student'],
     typespeed: 70,
-    backSpeed:70,
+    backSpeed: 70,
     backDelay: 1000,
     loop: true,
 });
